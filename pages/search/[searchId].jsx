e03ec439f7d7e7c62b36908d6952d6ae13c25202
Jobandeep Singh <126638427+Joban2109@users.noreply.github.com>
2024-04-14 23:37:49 +0530

    Add files via upload

---
+++ pages/search/[searchId].jsx
@@ -0,0 +1,73 @@
+import { useRouter } from "next/router";
+import React from "react";
+import Head from "next/head";
+import MainLayout from "../../components/layout/MainLayout";
+import SearchCard from "../../components/small-components/SearchCard";
+
+export const getServerSideProps = async (context) => {
+  const { searchId } = context.query;
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/search?keyw=${searchId}`,
+  );
+
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+function SearchPage({ data }) {
+  const router = useRouter();
+  const { searchId } = router.query;
+
+  return (
+    <>
+      <Head>
+        <title>{"Search Results For: " + searchId + " - Munchyroll"}</title>
+        <meta
+          name="description"
+          content="An ad-free anime streaming website aimed at minimality and responsive design. Share this with friends!"
+        />
+        <meta
+          property="og:title"
+          content={"Search Results: " + searchId + " - Munchyroll"}
+        />
+        <meta
+          property="og:description"
+          content="An ad-free anime streaming website aimed at minimality and responsive design. Share this with friends!"
+        />
+        <meta name="theme-color" content="#C4AD8A" />{" "}
+        {/* Maybe change this to scan image and return main color */}
+        <link rel="manifest" href="public/manifest.json" />
+      </Head>
+      <MainLayout useHead={false}>
+
+        {data && (
+          <>
+            <div className=" ">
+              <h1 className=" text-2xl font-bold">Search Results for: {searchId}</h1>
+
+              {data.length === 0 && (
+                <div className=" mt-10 text-2xl ">No Results Found</div>
+              )}
+            </div>
+            <div className=" mt-5 grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 auto-rows-[1fr] 2xl:grid-cols-7">
+              {data &&
+                data
+                  // .filter((anime) => !anime.animeTitle.toLowerCase().includes("dub"))
+                  .map((anime) => (
+                    <SearchCard key={anime.animeId} data={anime} />
+                  ))}
+            </div>
+          </>
+        )}
+      </MainLayout>
+    </>
+  );
+}
+
+export default SearchPage;
