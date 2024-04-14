d756a841b3be246fad3f4ef9f97a114c8558db42
Jobandeep Singh <126638427+Joban2109@users.noreply.github.com>
2024-04-14 23:34:09 +0530

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+import "../styles/globals.css";
+import "../styles/nprogress.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import React from "react";
+import ReactGA from "react-ga";
+
+import nprogress from "nprogress";
+
+ReactGA.initialize(process.env.NEXT_PUBLIC_GA_TRACKING_ID);
+
+import Router from "next/router";
+import GoogleAnalytics from "../components/GoogleAnalytics";
+Router.events.on("routeChangeStart", nprogress.start);
+Router.events.on("routeChangeError", nprogress.done);
+Router.events.on("routeChangeComplete", nprogress.done);
+function MyApp({ Component, pageProps }) {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GoogleAnalytics />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
---
+++ pages/home.js
@@ -0,0 +1,100 @@
+import React, { useEffect } from "react";
+import Head from "next/head";
+import MainLayout from "../components/layout/MainLayout";
+import ReleaseCard from "../components/small-components/ReleaseCard";
+import Card from "../components/small-components/Card";
+import ReactGA from "react-ga";
+
+export async function getServerSideProps() {
+  const popularResults = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/popular`,
+  );
+  const moviesResults = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/anime-movies`,
+  );
+  const recentResults = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/recent-release`,
+  );
+
+  const popular = await popularResults.json();
+  const movies = await moviesResults.json();
+  const recent = await recentResults.json();
+
+  return {
+    props: {
+      popular,
+      movies,
+      recent,
+    },
+  };
+}
+
+const Home = ({ popular, movies, recent }) => {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  });
+
+  return (
+    <>
+      <Head>
+        <title>Home - Munchyroll</title>
+        <meta
+          name="description"
+          content="An ad-free anime streaming website aimed at minimality and responsive design. Share this with friends!"
+        />
+        <meta property="og:title" content="Home - Munchyroll " />
+        <meta
+          property="og:description"
+          content="An ad-free anime streaming website aimed at minimality and responsive design. Share this with friends!"
+        />
+        <link rel="manifest" href="public/manifest.json" />
+        <meta name="theme-color" content="#C4AD8A" />{" "}
+        {/* Maybe change this to scan image and return main color */}
+      </Head>
+      <MainLayout useHead={false}>
+
+        {recent && (
+          <>
+            <h1 className=" text-2xl font-bold">Recent Anime</h1>
+            <div className="border-b-[2px] border-gray-600 pb-10 mt-5 grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 auto-rows-[1fr] 2xl:grid-cols-7">
+              {recent &&
+                recent.map((anime) => (
+                  <ReleaseCard key={anime.animeId} data={anime} />
+                ))}
+            </div>
+          </>
+        )}
+        <br></br>
+        <br></br>
+        <br></br>
+        {popular && (
+          <>
+            <h1 className=" text-2xl font-bold">Popular Anime</h1>
+            <div className="border-b-[2px] border-gray-600 pb-10 mt-5 grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 auto-rows-[1fr] 2xl:grid-cols-7">
+              {popular &&
+                popular.map((anime) => (
+                  <Card key={anime.animeId} data={anime} />
+                ))}
+            </div>
+          </>
+        )}
+        <br></br>
+        <br></br>
+        <br></br>
+        {movies && (
+          <>
+            <h1 className=" text-2xl font-bold">Anime Movies</h1>
+            <div className=" mt-5 grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 auto-rows-[1fr] 2xl:grid-cols-7">
+              {movies &&
+                movies.map((anime) => (
+                  <Card key={anime.animeId} data={anime} />
+                ))}
+            </div>
+          </>
+        )}
+      </MainLayout>
+    </>
+  );
+};
+
+export default Home;
